9cb159a2d8d724dbb3aadc1ae92cd54d95df5613
ludmila <malia.ludmila.sahraoui@ensia.edu.dz>
2024-12-04 22:12:32 +0100

    notification object def

---
+++ lib/data/model/notification_obj.dart
@@ -0,0 +1,31 @@
+import 'package:medi_mind/data/model/reminder.dart';
+
+class NotificationObj {
+  final Reminder data; // The associated reminder data
+  final bool taskValue; // Task completion status : true medication taken else false
+  final double notifTime; // the value is in minutes
+
+  const NotificationObj({
+    required this.data,
+    required this.taskValue,
+    required this.notifTime
+  });
+
+  /// Factory constructor to create a NotificationObj from a JSON map
+  factory NotificationObj.fromJson(Map<String, dynamic> json) {
+    return NotificationObj(
+      data: Reminder.fromJson(json['data'] as Map<String, dynamic>),
+      taskValue: json['taskValue'] as bool,
+       notifTime: json['notifTime'] as double,
+    );
+  }
+
+  /// Method to convert a NotificationObj instance into a JSON map
+  Map<String, dynamic> toJson() {
+    return {
+      'data': data.toJson(),
+      'taskValue': taskValue,
+       'notifTime': notifTime,
+    };
+  }
+}
