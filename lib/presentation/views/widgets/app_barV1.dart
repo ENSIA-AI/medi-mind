39bc87a98b997b29d1043a9eafd33377a1003930
ludmila <malia.ludmila.sahraoui@ensia.edu.dz>
2024-12-04 20:56:08 +0100

    app bar style 1 added

---
+++ lib/presentation/views/widgets/app_barV1.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:medi_mind/themes/colors.dart';
+
+class AppBarV1 extends StatelessWidget implements PreferredSizeWidget {
+  const AppBarV1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      actions: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: GestureDetector(
+            onTap: () {
+              debugPrint("Notification icon tapped");
+            },
+            child: const Icon(
+              Icons.notifications_rounded,
+              size: 24,
+              color: PRIMARY_BLUE,
+            ),
+          ),
+        ),
+        const Spacer(),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15.0),
+          child: GestureDetector(
+              onTap: () {
+                debugPrint("Settings icon tapped");
+              },
+              child: const Icon(
+                Icons.settings,
+                size: 24,
+                color: PRIMARY_BLUE,
+              )),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
