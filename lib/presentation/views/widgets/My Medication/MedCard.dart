6e296ffed77e2ebbdd872227d5750e16d2dce020
nada kechoud <nada.kechoud@ensia.edu.dz>
2024-12-06 20:34:38 +0100

    updated the medication card widget

--- lib/presentation/views/widgets/My Medication/MedCard.dart
+++ lib/presentation/views/widgets/My Medication/MedCard.dart
@@ -1,61 +1,91 @@
 import 'package:flutter/material.dart';
 import 'package:medi_mind/themes/styles.dart';
 import 'package:medi_mind/themes/colors.dart';
+import 'package:medi_mind/views/medication_detail_screen.dart';
+import 'package:medi_mind/data/model/medication.dart';
+
+
 
 class MedCard extends StatelessWidget {
-  const MedCard({super.key, required this.title, required this.subtitle, required this.picpath});
+  const MedCard({
+    super.key,
+    required this.medication
+  });
 
-  final String title;
-  final String subtitle;
-  final String picpath;
-
+  final Medication medication;
 
   @override
   Widget build(BuildContext context) {
     return Container(
+
       alignment: Alignment.center,
-      padding:  const EdgeInsets.fromLTRB(20, 10, 10, 10),
+      padding: const EdgeInsets.fromLTRB(20, 10, 10, 10),
       height: 100,
-      margin: const EdgeInsets.symmetric(vertical:  6.0, horizontal: 8),
+      margin: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 8),
       decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(18),
-      color:PRIMARY_BLUE,
-      boxShadow: [  BoxShadow(
-        color: const Color.fromARGB(78, 0, 0, 0), // shadow color
-        blurRadius: 2, // blur
-        offset: const Offset(0, 4), // shadow position
-      ),]
+        borderRadius: BorderRadius.circular(18),
+        color: PRIMARY_BLUE,
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(78, 0, 0, 0), // Shadow color
+            blurRadius: 2, // Blur radius
+            offset: const Offset(0, 4), // Shadow offset
+          ),
+        ],
       ),
-      child: 
-             Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.center,
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          //medicine picture
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              medication.picpath,
+              fit: BoxFit.cover,
+              width: 70,
+              height: 70,
+            ),
+          ),
+
+          const SizedBox(width: 30),
+
+         //text
+          Expanded(
+            child: 
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ClipRRect(
-                borderRadius: BorderRadius.circular(10),   
-                child: Image.asset(picpath, fit: BoxFit.cover , width:70, height: 70,),),
-                const SizedBox(width: 30,),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text( title, style: BEIGE_SUBHEADING_TEXT_STYLE, ),
-                       const SizedBox(width: 8,),
-                      Text( subtitle, style: WHITE_REGULAR_TEXT_STYLE , ),   
-                    ],
-                        ),
+                Text(
+                  medication.name,
+                  style: BEIGE_SUBHEADING_TEXT_STYLE,
                 ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Icon(Icons.more_vert, color: const Color.fromARGB(255, 255, 255, 255)),
-                  ],
-                ),        
-                        ],
-                      )
-               
-                      );
-                                     }
+                const SizedBox(height: 8),
+                Text(
+                  '${medication.type}  ${medication.dose}',
+                  style: WHITE_REGULAR_TEXT_STYLE,
+                ),
+              ],
+            ),
+          ),
+
+          //three dots 
+          InkWell(
+                  onTap: () {
+                    showModalBottomSheet(
+                      context: context,
+                      isScrollControlled: true, 
+                      backgroundColor: const Color.fromARGB(0, 255, 255, 255), 
+                      builder: (context) {
+                        return MedicineDetails(medobj: medication);}, //return the associated medication detail page
+                        );
+                      },
+                  child: const Icon(Icons.more_vert, color: Colors.white),
+                ),
+              ]
+            )
+          );
+  }
 }
